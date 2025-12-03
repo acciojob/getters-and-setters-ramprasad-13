45b97cacd295b9a1233fcc0044eef857adc7c73e
Pade venkata ramprasad <128288202+ramprasad-13@users.noreply.github.com>
2025-12-03 09:45:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,48 @@
-//complete this code
 class Person {
-	constructor(name, age){
-		this._name = name
-		this._age = age
-	}
-	get name(){
-		return this._name
-	}
+  constructor(name, age) {
+    // We use underscores (_) to store the actual data
+    // to avoid infinite loops with getters/setters
+    this._name = name;
+    this._age = age;
+  }
 
-	set name(name){
-		this._name = name
-	}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+  
+  // Setter for name (Added per "Notes" requirement)
+  set name(name) {
+    this._name = name;
+  }
 
-	get age(){
-		return this._age
-	}
-	
-	set age(age) {
-		this._age=age
-	}
+  // Getter for age (Added so we can read the age)
+  get age() {
+    return this._age;
+  }
+
+  // Setter for age
+  set age(age) {
+    this._age = age;
+  }
 }
 
 class Student extends Person {
-	study(){
-		console.log(`${this.name} is studying}`)
-	}
+  // No constructor needed; it automatically inherits (name, age) from Person
+  
+  study() {
+    // 1. Use console.log as requested
+    // 2. Use 'this.name' (no brackets) because it's a getter
+    console.log(`${this.name} is studying`);
+  }
 }
 
 class Teacher extends Person {
-	teach(){
-		console.log(`${this.name} is teaching`)
-	}
+  // No constructor needed here either
+
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
 
 // Do not change the code below this line
